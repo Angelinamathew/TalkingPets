460c50a08c5228ace4baea22c3766f11e4611f7f
Angelinamathew <angelinamathew707@gmail.com>
2024-03-23 21:27:52 -0400

    Finished

--- src/test/java/io/zipcoder/polymorphism/PetTest.java
+++ src/test/java/io/zipcoder/polymorphism/PetTest.java
@@ -1,4 +1,47 @@
 package io.zipcoder.polymorphism;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PetTest {
+    @Test
+    public void testPetConstructor(){
+        String expectedName = "name";
+        int expectedAge = 0;
+
+        Pet pet = new Pet();
+        Assert.assertNotNull(pet);
+    }
+    @Test
+    public void testPetConstructorNameAge(){
+        String expectedName = "name";
+        int expectedAge = 0;
+
+        Pet pet = new Pet(expectedName, expectedAge);
+        Assert.assertEquals(expectedName, pet.getName());
+        Assert.assertEquals(expectedAge, pet.getAge());
+    }
+    @Test
+    public void testPetConstructorName(){
+        String expectedName = "name";
+
+        Pet pet = new Pet(expectedName);
+        Assert.assertEquals(expectedName, pet.getName());
+    }
+    @Test
+    public void testPetConstructorAge(){
+        int expectedAge = 0;
+
+        Pet pet = new Pet(expectedAge);
+        Assert.assertEquals(expectedAge, pet.getAge());
+    }
+    @Test
+    public void testPetSpeak(){
+        String expectedName = "name";
+        int expectedAge = 0;
+
+        Pet pet = new Pet();
+        String expectedSpeak = "woooo";
+        Assert.assertEquals(expectedSpeak, pet.speak());
+    }
 }
